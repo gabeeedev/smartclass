c1a79b7c50bfa4dd87cd749ec1c9238fced1bf1d
Gabeee <sz.gabeee@gmail.com>
2020-05-29 10:20:03 +0200

    Settings fix

--- api/services/course_settings_edit.php
+++ api/services/course_settings_edit.php
@@ -12,10 +12,9 @@ require_once "../util/course.php";
 require_once "../util/util.php";
 
 loginRedirect();
-if (checkPostData(["courseTitle","publicCourse","courseId"]) && asTeacher()) {
+if (checkPostData(["courseTitle"]) && asTeacher()) {
     $data = [
         "title" => $_POST["courseTitle"],
-        "isPublic" => $_POST["publicCourse"] == "true" ? 1 : 0,
         "status" => intval($_POST["courseStatus"])
     ];
     sql_update_by_id("courses",$data,"courseId",$_SESSION["course"]["id"]);
